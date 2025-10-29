9b3917dcd63ab9bc6dbf9bf8b069d53ab4caf7d7
Anastasiya Titova <atitova1401@gmail.com>
2025-10-29 12:59:59 +0300

    update and delete files

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -16,6 +16,7 @@
       </div>
     </div>
   </div>
+  <RouterView />
 </template>
 
 <script setup>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store';
+import { router, ROUTES} from '@/router/index.js'
 
-createApp(App).use(store).mount('#app')
+const routes = {
+  install(app, options) {
+    console.log(ROUTES)
+    app.config.globalProperties.$routes = ROUTES
+  }
+}
+
+createApp(App)
+  .use(router)
+  .use(routes)
+  .use(store)
+  .mount('#app')
