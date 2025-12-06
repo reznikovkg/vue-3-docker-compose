8545635df3b939b1035e468a40a524af52a83d2c
arinaburkova <109163874+arinaburkova@users.noreply.github.com>
2025-12-06 20:06:04 +0300

    Update DictophoneEditorPage.vue

--- vue/src/components/pages/DictophoneEditorPage.vue
+++ vue/src/components/pages/DictophoneEditorPage.vue
@@ -158,7 +158,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, onUnmounted, watch, nextTick } from 'vue'
+import { ref, computed, onMounted, onUnmounted, nextTick } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
