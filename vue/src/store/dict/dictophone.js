7f8cd18c67833b783eae31cc6c0f4c43db4a0df0
arinaburkova <109163874+arinaburkova@users.noreply.github.com>
2025-12-06 20:05:01 +0300

    Update dictophone.js

--- vue/src/store/dict/dictophone.js
+++ vue/src/store/dict/dictophone.js
@@ -18,8 +18,8 @@ export const DictophoneModule = {
     getRecordings: (state) => state.recordings,
     getCurrentAudioUrl: (state) => state.currentAudioUrl,
     getRecordingsCount: (state) => state.recordings.length,
-    getHasRecordings: (state) => state.recordings.length > 0
-  },
+    getHasRecordings: (state) => state.recordings.length > 0,
+   },
 
   mutations: {
     SET_RECORDING(state, isRecording) {
@@ -200,7 +200,7 @@ export const DictophoneModule = {
     },
 
  updateRecording({ commit, state }, { id, url, name }) {
-  const item = state.recordings.find(rec => rec.id === id)    
+  const item = state.recordings.find(rec => rec.id === id)
   const index = state.recordings.findIndex(rec => rec.id === id)
   URL.revokeObjectURL(state.recordings[index].url)
   commit('UPDATE_RECORDING', {
