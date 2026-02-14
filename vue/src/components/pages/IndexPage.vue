8f3a974dc6451e5d2a38f587b9fabaf59e02de57
AlexAstakhov56 <aleks.astakhov.2015@mail.ru>
2026-02-14 19:17:53 +0300

    create custom button

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -1,17 +1,27 @@
 <template>
-  Index
-
   <div>
-    <RouterLink :to="{ name: $routes.EXAMPLE }">
-     To Example
-    </RouterLink>
+    <CustomButton />
+    <CustomButton type="red">Красная кнопка</CustomButton>
+    <CustomButton type="blue">Синяя кнопка</CustomButton>
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import CustomButton from "../ui/CustomButton.vue";
 
+export default {
+  name: "IndexPage",
+  components: {
+    CustomButton,
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
+body {
+  background-color: #fff;
+  display: block;
+  margin-top: 100px;
+  color: #000;
+}
 </style>
