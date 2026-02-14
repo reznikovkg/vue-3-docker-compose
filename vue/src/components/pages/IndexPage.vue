1d0ac6ebc2f3ead217cc2161f405a19ad6221b2d
AlexAstakhov56 <aleks.astakhov.2015@mail.ru>
2026-02-14 19:18:24 +0300

    added people list

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -4,6 +4,16 @@
     <CustomButton type="red">Красная кнопка</CustomButton>
     <CustomButton type="blue">Синяя кнопка</CustomButton>
   </div>
+  <div>
+    <input type="text" v-model="name" placeholder="Name" />
+    <input type="text" v-model="age" placeholder="Age" />
+    <CustomButton @click="() => addPerson()">Add</CustomButton>
+
+    <p>People list:</p>
+    <div v-for="p in people" :key="p.name">
+      <p>{{ p.name }} - {{ p.age }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +24,22 @@ export default {
   components: {
     CustomButton,
   },
+  data() {
+    return {
+      people: [],
+      name: "",
+      age: 0,
+    };
+  },
+  methods: {
+    addPerson() {
+      this.people.push({
+        name: this.name,
+        age: this.age,
+      });
+      ((this.name = ""), (this.age = 0));
+    },
+  },
 };
 </script>
 
@@ -23,5 +49,10 @@ body {
   display: block;
   margin-top: 100px;
   color: #000;
+}
+input {
+  padding: 5px;
+  font-size: 16px;
+  margin: 0 10px;
 }
 </style>
