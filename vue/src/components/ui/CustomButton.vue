757c6c1beeba39d76159654c3942b88f5d86d18d
Paspartoshka <deolineww@gmail.com>
2026-02-17 18:32:03 +0300

    изменения с занятия

---
+++ vue/src/components/ui/CustomButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <button class="c-button" :class="'c-button--' + type">
+    <div class="c-button__icon">
+      + {{text}}
+    </div>
+    <slot>Кнопка</slot>
+    <template v-if="$slots.count">
+      (<slot name="count">0</slot>
+    </template>
+    <input v-model="text" type="text" @click.stop @input="() => check()">
+  </button>
+
+</template>
+
+<script>
+export default {
+  name: "CustomButton",
+  emits: ['len'],
+  props: {
+    type: {
+      default: 'default',
+      type: String
+    }
+  },
+  data () {
+    return {
+      text: ' '
+    }
+  },
+  methods: {
+    check () {
+      if (this.text.length > 10){
+        this.$emit('len', this.text.length)
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+.c-button {
+  border: 1px solid #181818;
+  background: #2c3e50;
+  color: white;
+  font-size: 12px;
+  padding: 5px;
+  margin: 5px;
+  display: flex;
+
+  &--red {
+    border: 1px solid #a52828;
+    background: #e32525;
+  }
+
+  &__icon {
+    color: yellow;
+  }
+}
+</style>
