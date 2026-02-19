4afd5b66fa50173de64d6417dc0d53c65b6bfb39
brodyaga333 <sergey.kozhanov.1999@gmail.com>
2026-02-19 22:16:02 +0300

    Add button

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -1,17 +1,65 @@
 <template>
-  Index
-
   <div>
-    <RouterLink :to="{ name: $routes.EXAMPLE }">
-     To Example
-    </RouterLink>
+    <div>
+      R: {{ count }}
+    </div>
+    <div>
+      2PR: {{ c2pr }}
+    </div>
+    <div>
+      TEXT: {{ lastText }}
+    </div>
+    <div>
+      LEN: {{ lastLen }}
+    </div>
+
+    <CustomButton type="red" @click="() => addCount(5)">+5</CustomButton>
+
+    <CustomButton @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">
+      +10
+      <template #count>
+        999
+      </template>
+    </CustomButton>
+
+    <CustomButton @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">+10</CustomButton>
+    <CustomButton type="disabled" @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">+10</CustomButton>
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import CustomButton from '@/components/ui/CustomButton.vue'
 
+export default {
+  name: 'IndexPage',
+  components: {
+    CustomButton
+  },
+  data () {
+    return {
+      count: 10,
+      lastLen: null,
+      lastText: ''
+    }
+  },
+  computed: {
+    c2pr () {
+      return 2 * Math.PI * this.count
+    }
+  },
+  methods: {
+    addCount (v = 10) {
+      this.count += v
+    },
+    onLen (v) {
+      this.lastLen = v
+    },
+    onText (t) {
+      this.lastText = t
+    }
+  }
+}
 </script>
 
-<style scoped>
-
+<style lang="scss">
 </style>
