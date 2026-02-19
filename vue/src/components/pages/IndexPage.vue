8bf8d6fbf2136a755c2605c67a84396ad1c19a1f
volchica-luta <lysachevaolga@gmail.com>
2026-02-19 14:40:18 +0300

    Халтурина Ольга, 61гр, демонстрация работы с GitHub

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -6,10 +6,72 @@
      To Example
     </RouterLink>
   </div>
+
+  <div>
+   R: {{ count }} 
+  </div> 
+
+  <div>
+   2piR: {{ c2pr }}
+  </div>
+
+  <div>
+  типа суперкнопки и суперзначение:
+  {{count}} <!--Найди переменную с именем count в нашем коде (в <script>) и подставь сюда её текущее значение-->
+  <button @click="() => addCount()">+</button> <!--Слушай клик @click по этой кнопке и, когда он произойдет, выполни код в кавычках-->
+  <button @click="() => addCount(5)">добавляем 5</button>
+  </div>
+
+
+<div>
+  кастом кнопка:
+  <CustomButton @click="() => addCount(10)">добавляем 10</CustomButton>
+  <CustomButton @click="() => addCount(10)"></CustomButton>
+
+  <CustomButton @click="() => addCount(10)">
+  123
+  <template #count>
+  999
+  </template>
+  </CustomButton>
+  </div>
+
+
+
+  <div> <div>
+  <CustomButton :type="'red'" @click="() => addCount(5)">кнопочка+5с типом</CustomButton> <!--А КНОПОЧКА ПОЧЕМУ-ТО НЕ КРАСНАЯ(((-->
+  </div></div>
+
 </template>
 
-<script setup lang="ts">
+<script>
 
+import CustomButton from '../ui/CustomButton.vue'
+
+export default {
+    name: 'IndexPage',
+    components:
+    {
+        CustomButton
+    },
+    data(){
+        return {
+            count: 10
+        }
+    },
+computed: {
+c2pr(){
+    return 2 * Math.PI * this.count
+}
+},
+methods:
+{
+    addCount(v = 10)
+    {
+        this.count += v
+    }
+}
+}
 </script>
 
 <style scoped>
---
+++ vue/src/components/ui/CustomButton.vue
@@ -0,0 +1,40 @@
+<template>
+
+<button class="c-button" :class="'c-button--' + type">
+<slot>Кнопочка-наша</slot>
+<template v-if=" $slots.count"> <!--если бы строка ниже не была обернута в template, то в скобочках на кнопочках были нули-->
+(<slot name = "count">0</slot>)
+</template>
+
+</button>
+
+</template>
+
+<script>
+export default{
+    name: 'CustomButton',
+    props: {
+        type:{
+            default: 'default',
+            type: String
+            
+        }
+    }
+}
+</script>
+
+<style lang = "scss">
+.c-button {
+    border: 1px solid #00d199;
+    background: #009200;
+    color: white;
+    font-size: 12px;
+    padding: 5px;
+    margin: 5px;
+
+    &--red {
+          border: 1px solid #ff7d7d;
+          background: #5a0c00;
+    }
+}
+</style>
