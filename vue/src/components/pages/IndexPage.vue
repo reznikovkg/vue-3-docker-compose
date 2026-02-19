56ef07522a4e0e8c6ea2422e8ab249aa4adea50f
Nikita Kutsaev <nikita.kutsaev55@yandex.ru>
2026-02-19 21:47:06 +0300

    Test

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -1,17 +1,65 @@
 <template>
-  Index
-
   <div>
-    <RouterLink :to="{ name: $routes.EXAMPLE }">
-     To Example
-    </RouterLink>
+    <div>R: {{ count }}</div>
+    <div>2PR: {{ c2pr }}</div>
+    <div>Diameter: {{ diameter }}</div>
+    <div>Square : {{ square }}</div>
+
+    <CustomButton :type="'red'" @click="() => addCount(5)">+5</CustomButton>
+    <CustomButton @click="() => addCount(10)">+10</CustomButton>
+    <CustomButton @click="() => removeCount(5)">-5</CustomButton>
+    <CustomButton @click="() => removeCount(10)">-10</CustomButton>
+    <CustomButton
+      :showInput="true"
+      @click="() => addCount(10)"
+      @len="(v) => check(v)"
+    >
+      123
+      <template #count> 999 </template>
+    </CustomButton>
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import CustomButton from "@/components/ui/CustomButton.vue";
 
+export default {
+  name: "IndexPage",
+  components: {
+    CustomButton,
+  },
+  data() {
+    return {
+      count: 10,
+    };
+  },
+  computed: {
+    c2pr() {
+      return 2 * Math.PI * this.count;
+    },
+    diameter() {
+      return 2 * this.count;
+    },
+    square() {
+      return Math.PI * this.count * this.count;
+    },
+  },
+  methods: {
+    addCount(v = 10) {
+      this.count += v;
+    },
+    removeCount(v = 10) {
+      this.count -= v;
+      if (this.count < 0) {
+        alert("Радиус не может быть отрицательным");
+        this.count = 0;
+      }
+    },
+    check(v) {
+      alert(v);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
