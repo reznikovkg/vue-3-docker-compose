6d30eb3ab883de7a794ecd57a137ecfd6ed2fa64
AnnaGribanova <griban0va.a@yahoo.com>
2026-02-21 12:28:39 +0300

    Counter and button

---
+++ vue/src/components/pages/CounterAndButtonPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <RouterLink :to="{ name: $routes.INDEX }" >To Index</RouterLink>
+
+    <h1>Counter : {{ count }}</h1>
+
+    <button @click="inc">Увеличить
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const count = computed(() => store.getters.getCount)
+
+const inc = () => {
+  store.dispatch('runIncrement', 1)
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,13 +2,20 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import IndexPage from './../components/pages/IndexPage.vue'
 import ExamplePage from './../components/pages/ExamplePage.vue'
+import CounterAndButtonPage from './../components/pages/CounterAndButtonPage.vue'
 
 export const ROUTES = {
+  COUNTERANDBUTTON: 'COUNTERANDBUTTON',
   EXAMPLE: 'EXAMPLE',
   INDEX: 'INDEX',
 }
 
 const routes = [
+  {
+    name: ROUTES.COUNTERANDBUTTON,
+    path: '/counterbutton',
+    component: CounterAndButtonPage
+  },
   {
     name: ROUTES.EXAMPLE,
     path: '/example',
