7a8f981099719220bf4b6f6d24bb6e758b491a3a
Danillip <danillip2@inbox.ru>
2026-02-24 16:04:22 +0300

    create base

---
+++ vue/src/components/BubbleGame.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="c-game">
+    <slot name="start" :onStart="handleStart">
+      <button type="button" class="c-game__start" @click="() => handleStart()">
+        <slot name="start-label"></slot>
+      </button>
+    </slot>
+
+    <div class="c-game__field"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BubbleGame',
+
+  props: {
+    colorsCount: {
+      type: Number,
+      default: 3
+    },
+    targetColor: {
+      type: String,
+      default: 'red'
+    },
+    intensity: {
+      type: Number,
+      default: 1
+    },
+    scoreHit: {
+      type: Number,
+      default: 1
+    },
+    scoreMiss: {
+      type: Number,
+      default: -5
+    },
+    onStart: {
+      type: Function,
+      default: null
+    }
+  },
+
+  emits: ['finish', 'update:score'],
+
+  data() {
+    return {
+      score: 0,
+      bubbles: [],
+      isRunning: false
+    }
+  },
+
+  methods: {
+    handleStart() {
+      this.isRunning = true
+
+      if (typeof this.onStart === 'function') {
+        this.onStart()
+      }
+
+      this.$emit('update:score', this.score)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.c-game {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+.c-game__start {
+  width: fit-content;
+}
+
+.c-game__field {
+  min-height: 320px;
+  border: 1px solid #d9d9d9;
+  border-radius: 8px;
+}
+</style>
--- vue/src/components/pages/ExamplePage.vue
+++ vue/src/components/pages/ExamplePage.vue
@@ -1,19 +1,30 @@
 <template>
-  Example STORE {{ count }}
-  <RouterLink :to="{ name: $routes.INDEX }" >To Index</RouterLink>
-  <input v-model="value" type="number">
-  <input v-model="timeout" type="number">
-  <button @click="() => inc()">+</button>
-  <button @click="() => setValue()">SET</button>
-
   <div>
-    <div v-for="i in list" :key="i">{{ i }}</div>
+    <div>Example STORE {{ count }}</div>
+    <RouterLink :to="{ name: $routes.INDEX }">To Index</RouterLink>
+
+    <div>
+      <input v-model="value" type="number">
+      <input v-model="timeout" type="number">
+      <button @click="() => inc()">+</button>
+      <button @click="() => setValue()">SET</button>
+    </div>
+
+    <div>
+      <div v-for="i in list" :key="i">{{ i }}</div>
+    </div>
+
+    <BubbleGame>
+      <template #start-label>Bubble</template>
+    </BubbleGame>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
+import BubbleGame from '@/components/BubbleGame.vue'
+
 const store = useStore()
 const count = computed(() => store.getters.getCount)
 const list = computed(() => store.getters['list/getList'])
