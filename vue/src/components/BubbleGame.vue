1b7190c1eaf934b85ba897845d0cb7e5d9d0cbd5
Danillip <danillip2@inbox.ru>
2026-02-24 16:59:17 +0300

    render bubbles

--- vue/src/components/BubbleGame.vue
+++ vue/src/components/BubbleGame.vue
@@ -6,7 +6,17 @@
       </button>
     </slot>
 
-    <div class="c-game__field"></div>
+    <div class="c-game__field">
+      <button
+        v-for="bubble in bubbles"
+        :key="bubble.id"
+        type="button"
+        class="c-game__bubble"
+        :class="'c-game__bubble--' + bubble.color"
+        :style="{ top: bubble.top + 'px', left: bubble.left + 'px' }"
+        @click="() => handleBubbleClick(bubble.id)"
+      ></button>
+    </div>
   </div>
 </template>
 
@@ -46,7 +56,12 @@ export default {
   data() {
     return {
       score: 0,
-      bubbles: [],
+      bubbles: [
+        { id: 1, color: 'red', top: 50, left: 60 },
+        { id: 2, color: 'blue', top: 140, left: 220 },
+        { id: 3, color: 'green', top: 240, left: 120 },
+        { id: 4, color: 'red', top: 320, left: 360 }
+      ],
       isRunning: false
     }
   },
@@ -60,6 +75,11 @@ export default {
       }
 
       this.$emit('update:score', this.score)
+    },
+
+    handleBubbleClick(id) {
+      const clickedBubbleId = id
+      void clickedBubbleId
     }
   }
 }
@@ -77,9 +97,31 @@ export default {
 }
 
 .c-game__field {
+  position: relative;
   width: 640px; // Пока будет так
   height: 480px;
   border: 1px solid #d9d9d9;
   border-radius: 8px;
+}
+
+.c-game__bubble {
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  border: 0;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.c-game__bubble--red {
+  background: #ff4d4f;
+}
+
+.c-game__bubble--blue {
+  background: #4096ff;
+}
+
+.c-game__bubble--green {
+  background: #73d13d;
 }
 </style>
