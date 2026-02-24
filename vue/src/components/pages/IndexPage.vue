c5004c4a18e0b70026ad7e9cd749a418d0f616ec
Danillip <danillip2@inbox.ru>
2026-02-24 15:35:58 +0300

    Supplement to the 21.02 lecture

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -1,65 +1,74 @@
 <template>
   <div>
-    <div>
-      R: {{ count }}
-    </div>
-    <div>
-      2PR: {{ c2pr }}
-    </div>
-    <div>
-      TEXT: {{ lastText }}
-    </div>
-    <div>
-      LEN: {{ lastLen }}
-    </div>
+    <CustomButton :type="'red'" @click="() => addCount(5)">+5</CustomButton>
 
-    <CustomButton type="red" @click="() => addCount(5)">+5</CustomButton>
-
-    <CustomButton @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">
-      +10
+    <CustomButton @click="() => addCount(10)" @len="(v) => check(v)">
+      123
       <template #count>
         999
       </template>
     </CustomButton>
+    <CustomButton @click="() => addCount(10)"></CustomButton>
+    <CustomButton @click="() => addCount(10)" />
 
-    <CustomButton @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">+10</CustomButton>
-    <CustomButton type="disabled" @click="() => addCount(10)" @len="(v) => onLen(v)" @text="(t) => onText(t)">+10</CustomButton>
+    <div>
+      <template v-for="(i, index) in getListPower" :key="index">
+        <div :class="[]">
+          {{ i }}, {{ index }}
+        </div>
+      </template>
+      <!--
+      <div v-for="i in getList" :key="i.t ?? i">
+        {{ i }}
+      </div>
+      -->
+    </div>
   </div>
 </template>
 
 <script>
 import CustomButton from '@/components/ui/CustomButton.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
-  name: 'IndexPage',
   components: {
     CustomButton
   },
   data () {
     return {
       count: 10,
-      lastLen: null,
-      lastText: ''
+      params: []
     }
   },
   computed: {
+    ...mapGetters('list', [
+      'getList',
+      // 'getListPower'
+      'getListPower'
+    ]),
     c2pr () {
       return 2 * Math.PI * this.count
     }
   },
+  mounted () {
+    console.log(this.getList[0].t)
+    this.getList[0].t = 90
+  },
   methods: {
+    ...mapActions('list', [
+      'setList'
+    ]),
     addCount (v = 10) {
       this.count += v
+      // const p = [...this.getListPower]
+      // p.push(v)
+      // this.setList(p)
+
+      // asdas
     },
-    onLen (v) {
-      this.lastLen = v
-    },
-    onText (t) {
-      this.lastText = t
+    check (v) {
+      alert(v)
     }
   }
 }
 </script>
-
-<style lang="scss">
-</style>
