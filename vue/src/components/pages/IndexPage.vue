483f0d09e969ec073b10f9e9fd957fcf0dad7a8a
Terragul-Mason <artem.galenin@yandex.ru>
2026-02-26 18:50:00 +0300

    Добавил генерацию случайных слоев

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -12,7 +12,46 @@ export default {
   components: { Flask },
   data () {
     return {
-      flasks: [[], [], [], [], [], []]
+      flasks: [],
+      MAX_LAYERS: 4,
+      COLORS: ['red', 'blue', 'green', 'yellow']
+    }
+  },
+  mounted () {
+    this.generateGame()
+  },
+  methods: {
+    generateGame () {
+      this.flasks = []
+
+      const TOTAL = this.COLORS.length + 2
+
+      for (let i = 0; i < TOTAL; i++) {
+        this.flasks.push([])
+      }
+
+      const all = []
+
+      for (let i = 0; i < this.COLORS.length; i++) {
+        for (let j = 0; j < this.MAX_LAYERS; j++) {
+          all.push(this.COLORS[i])
+        }
+      }
+
+      for (let i = all.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        const tmp = all[i]
+        all[i] = all[j]
+        all[j] = tmp
+      }
+
+      while (all.length > 0) {
+        const index = Math.floor(Math.random() * TOTAL)
+
+        if (this.flasks[index].length < this.MAX_LAYERS) {
+          this.flasks[index].push(all.pop())
+        }
+      }
     }
   }
 }
