ecf777b8cb6b0d38ad40c0d289b451116a938fe8
BrightSparkle <ekaterinamrch@gmail.com>
2026-03-04 21:03:48 +0300

    create pieces

--- vue/src/components/Field/FieldTable.vue
+++ vue/src/components/Field/FieldTable.vue
@@ -1,21 +1,29 @@
 <template>
   <table class="field-table">
     <tbody>
-      <tr v-for="row in this.getFieldSize" :key="row">
-        <td class="field-td" :style="{ '--grid-size': this.getFieldSize }" v-for="col in this.getFieldSize" :key="col">
-        </td>
+      <tr v-for="(row, rowIdx) in grid" :key="rowIdx">
+        <td
+          v-for="(cell, colIdx) in row"
+          :key="colIdx"
+          class="field-td"
+          :class="{ piece: cell === 2 }"
+          :style="{ '--grid-size': getFieldSize }"
+        ></td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'FieldTable',
   computed: {
-    ...mapGetters('field', ['getFieldSize']),
+    ...mapGetters('field', ['getFieldSize', 'getField']),
+    grid() {
+      return this.getField || []
+    }
   }
 }
 </script>
@@ -26,10 +34,13 @@ export default {
   width: fit-content;
   table-layout: fixed;
 }
-
 .field-td {
   border: 0.5vmin solid #000000;
   width: calc(80vmin / var(--grid-size));
   height: calc(80vmin / var(--grid-size));
+  background-color: white;
+}
+.field-td.piece {
+  background-color: #ff9800;
 }
 </style>
