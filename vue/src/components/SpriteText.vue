bc24ef68e94974a603ee724ca3a2c5832cbfd063
S-o-K-o-L <rozhkov-aleksander.rohkov@yandex.ru>
2026-03-06 15:30:33 +0300

    sprite text mapping

---
+++ vue/src/components/SpriteText.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sprite-text">
+    <span
+        v-for="(char, index) in characters"
+        :key="index"
+        class="sprite-text__char"
+        :style="getCharStyle(char)"
+    />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  text: String,
+  config: Object
+})
+
+const characters = computed(() => props.text.split(''))
+
+const getCharStyle = (char) => {
+  const map = props.config.spriteMap[char.toUpperCase()] || props.config.spriteMap[' ']
+  if (!map) return {}
+
+  return {
+    display: 'inline-block',
+    width: `${map.width}px`,
+    height: `${map.height}px`,
+    backgroundImage: `url(${props.config.spriteUrl})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: `-${map.x}px -${map.y}px`,
+    backgroundSize: 'auto',
+    verticalAlign: 'top'
+  }
+}
+</script>
+
+<style scoped lang="less">
+.sprite-text {
+  display: inline-flex;
+
+  &__char {
+    display: inline-block;
+    background-repeat: no-repeat;
+  }
+}
+</style>
---
+++ vue/src/configs/pixelSpriteConfig.js
@@ -0,0 +1,64 @@
+import spriteImage from '../assets/pixel-sprite.png'
+
+export const pixelSpriteConfig = {
+    spriteUrl: spriteImage,
+    fontSize: 32,
+    spriteMap: {
+        // 1-я строка спрайта
+        A: {x: 33, y: 20, width: 70, height: 100},
+        B: {x: 110, y: 20, width: 70, height: 100},
+        C: {x: 185, y: 20, width: 70, height: 100},
+        D: {x: 253, y: 20, width: 70, height: 100},
+        E: {x: 320, y: 20, width: 70, height: 100},
+        F: {x: 390, y: 20, width: 70, height: 100},
+        G: {x: 470, y: 20, width: 70, height: 100},
+        H: {x: 550, y: 20, width: 70, height: 100},
+        I: {x: 610, y: 20, width: 60, height: 100},
+        J: {x: 670, y: 20, width: 70, height: 100},
+        K: {x: 740, y: 20, width: 70, height: 100},
+        L: {x: 820, y: 20, width: 70, height: 100},
+        M: {x: 895, y: 20, width: 70, height: 100},
+        N: {x: 970, y: 20, width: 75, height: 100},
+
+        // 2-я строка спрайта
+        O: {x: 62, y: 110, width: 60, height: 108},
+        P: {x: 130, y: 110, width: 70, height: 108},
+        Q: {x: 200, y: 110, width: 70, height: 108},
+        R: {x: 270, y: 110, width: 70, height: 108},
+        S: {x: 350, y: 110, width: 70, height: 108},
+        T: {x: 425, y: 110, width: 70, height: 108},
+        U: {x: 505, y: 110, width: 70, height: 108},
+        V: {x: 592, y: 110, width: 75, height: 108},
+        W: {x: 680, y: 110, width: 90, height: 108},
+        X: {x: 785, y: 110, width: 70, height: 108},
+        Y: {x: 870, y: 110, width: 70, height: 108},
+        Z: {x: 950, y: 110, width: 70, height: 108},
+
+        // 3-я строка спрайта
+        '1': {x: 175, y: 208, width: 50, height: 108},
+        '2': {x: 220, y: 208, width: 70, height: 108},
+        '3': {x: 300, y: 208, width: 70, height: 108},
+        '4': {x: 380, y: 208, width: 70, height: 108},
+        '5': {x: 460, y: 208, width: 60, height: 108},
+        '6': {x: 520, y: 208, width: 70, height: 108},
+        '7': {x: 600, y: 208, width: 70, height: 108},
+        '8': {x: 680, y: 208, width: 70, height: 108},
+        '9': {x: 760, y: 208, width: 70, height: 108},
+        '0': {x: 840, y: 208, width: 70, height: 108},
+
+        // 4-я строка спрайта
+        '.': {x: 185, y: 300, width: 30, height: 108},
+        ',': {x: 240, y: 300, width: 30, height: 108},
+        '!': {x: 280, y: 300, width: 40, height: 108},
+        '?': {x: 335, y: 300, width: 70, height: 108},
+        ';': {x: 410, y: 300, width: 30, height: 108},
+        '-': {x: 450, y: 300, width: 60, height: 108},
+        '+': {x: 510, y: 300, width: 70, height: 108},
+        '=': {x: 600, y: 300, width: 70, height: 108},
+        '…': {x: 690, y: 300, width: 70, height: 108},
+        '(': {x: 780, y: 300, width: 40, height: 108},
+        ')': {x: 835, y: 300, width: 40, height: 108},
+
+        ' ': {x: 1000, y: 320, width: 60, height: 108}
+    }
+}
