d6558eb76c9a209eb5bdeabc2b9d17def5821e41
rinchik0 <ariadna.kolupaeva@yandex.ru>
2026-03-07 05:12:55 +0300

    Визуализация выбора колбы

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,14 @@
-<!-- src/App.vue -->
 <template>
   <div id="app">
     <h1>Water Sort Puzzle</h1>
     <div class="flasks-container">
-      <Flask :layers="exampleLayers" />
-      <Flask :layers="[]" />
+      <Flask
+        v-for="(flask, index) in flasks"
+        :key="index"
+        :layers="flask.layers"
+        :is-selected="selectedFlaskIndex === index"
+        @click="handleFlaskClick(index)"
+      />
     </div>
   </div>
 </template>
@@ -19,11 +23,43 @@ export default {
   },
   data() {
     return {
-      exampleLayers: [
-        { color: 'red', percent: 25 },
-        { color: 'blue', percent: 30 },
-        { color: 'green', percent: 45 },
+      COLORS: ['red', 'blue', 'green', 'yellow', 'purple', 'orange'],
+      selectedFlaskIndex: null,
+      flasks: [
+        {
+          layers: [
+            { color: 'yellow', percent: 25 },
+            { color: 'blue', percent: 25 },
+            { color: 'yellow', percent: 25 },
+            { color: 'blue', percent: 25 }
+          ]
+        },
+        {
+          layers: [ 
+            { color: 'blue', percent: 25 },
+            { color: 'yellow', percent: 25 },
+            { color: 'blue', percent: 25 },
+            { color: 'yellow', percent: 25 }
+          ]
+        },
+        {
+          layers: []
+        }
       ]
+    }
+  },
+  methods: {
+    handleFlaskClick(index) {
+      console.log('Клик по колбе с индексом:', index)
+      if (this.selectedFlaskIndex === null) {
+        this.selectedFlaskIndex = index
+        return
+      }
+      if (this.selectedFlaskIndex === index) {
+        this.selectedFlaskIndex = null
+        return;
+      }
+      
     }
   }
 }
