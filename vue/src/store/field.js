6fc517e744cf585f9c147251e16b773eafe7170a
Zag207 <danilka.stepanov04@gmail.com>
2026-03-09 14:49:03 +0300

    bugFix: central cube position in field is 1 when game started

--- vue/src/store/field.js
+++ vue/src/store/field.js
@@ -48,10 +48,16 @@ export default {
           state.size += 1
       }
 
-      state.field = Array(state.size).fill(null).map(() => Array(state.size).fill(0))
+      state.field = Array(state.size).fill(null).map(
+            () => Array(state.size).fill(OBJECTS.NONE)
+        )
       state.gameActive = false
       state.currentPiece = null
       state.lastSide = null
+
+      let center = Math.floor(state.size / 2)
+
+      state.field[center][center] = OBJECTS.CENTRAL_CUBE
     },
     [MUTATIONS.CLEAR_FIELD]: (state) => {
       state.field = Array(state.size).fill(null).map(() => Array(state.size).fill(0))
