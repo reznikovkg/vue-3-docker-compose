28f62f990184997cd40457bb36cd688ba1c91518
Maxim Zaytsev <m3r1v3@gmail.com>
2026-03-09 15:44:25 +0300

    Добавляет содержимое компонента карты мира GameMap.vue

--- vue/src/components/GameMap.vue
+++ vue/src/components/GameMap.vue
@@ -1,12 +1,81 @@
 <template>
-  <div class="map">
+  <div class="game-map">
+    <div class="game-map__world"/>
   </div>
+
+  <Boat/>
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from 'vue'
+import { useStore } from 'vuex'
+import Boat from './Boat.vue'
+
+const store = useStore()
+
+const speed = 1
+
+const move = (e) => {
+  if (e.key === 'ArrowUp' || e.key === 'w') {
+    store.dispatch('game/moveBoat', {x: 0, y: -speed})
+    store.dispatch('game/setRowing', true)
+  }
+  if (e.key === 'ArrowLeft' || e.key === 'a') {
+    store.dispatch('game/moveBoat', {x: -speed, y: 0})
+    store.dispatch('game/setDirection', -1)
+    store.dispatch('game/setRowing', true)
+  }
+  if (e.key === 'ArrowDown' || e.key === 's') {
+    store.dispatch('game/moveBoat', {x: 0, y: speed})
+    store.dispatch('game/setRowing', true)
+  }
+  if (e.key === 'ArrowRight' || e.key === 'd') {
+    store.dispatch('game/moveBoat', {x: speed, y: 0})
+    store.dispatch('game/setDirection', 1)
+    store.dispatch('game/setRowing', true)
+  }
+}
+
+const stopMove = (e) => {
+  if (e.key === 'ArrowUp' || e.key === 'w') {
+    store.dispatch('game/setRowing', false)
+  }
+  if (e.key === 'ArrowLeft' || e.key === 'a') {
+    store.dispatch('game/setRowing', false)
+  }
+  if (e.key === 'ArrowDown' || e.key === 's') {
+    store.dispatch('game/setRowing', false)
+  }
+  if (e.key === 'ArrowRight' || e.key === 'd') {
+    store.dispatch('game/setRowing', false)
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', move)
+  window.addEventListener('keyup', stopMove)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', move)
+})
 
 </script>
 
 <style scoped>
+.game-map {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+  z-index: 1;
+  
+  background-color: rgb(50, 50, 100);
+}
 
+.game-map__world {
+  position: absolute;
+  width: 5000px;
+  height: 5000px;
+}
 </style>
