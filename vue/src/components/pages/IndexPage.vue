415f52f8e0bdd7af7d359f8b00c2ddb9297032ea
Maxim Zaytsev <m3r1v3@gmail.com>
2026-03-09 15:42:02 +0300

    Добавляет компоненты на страницу

--- vue/src/components/pages/IndexPage.vue
+++ vue/src/components/pages/IndexPage.vue
@@ -8,17 +8,17 @@
 </template>
 
 <script setup>
-  import GameMap from '../GameMap.vue';
-  import InfoBar from '../InfoBar.vue';
-  import Inventory from '../Inventory.vue';
-  import MiniGame from '../MiniGame.vue';
+import GameMap from '../GameMap.vue';
+import InfoBar from '../InfoBar.vue';
+import Inventory from '../Inventory.vue';
+import MiniGame from '../MiniGame.vue';
 </script>
 
 <style scoped lang="scss">
-  .layout {
-    width: 100%;
-    height: 100vh;
-    background-color: rgb(47, 108, 165);
-    overflow:hidden;
-  }
+.layout {
+  width: 100%;
+  height: 100%;
+  overflow:hidden;
+  position: relative;
+}
 </style>
