2c02bdc4baefb751375c1b06f27c83a4338410ff
GitStudent0-0 <237612774+GitStudent0-0@users.noreply.github.com>
2026-03-11 21:01:49 +0300

    lab1_stage1

---
+++ vue/src/data/levels.js
@@ -0,0 +1,10 @@
+export const levels = [
+  {
+    id: 1, 
+    path: [{x: 0, y: 30}, {x: 100, y: 30}, {x: 100, y: 100}],
+    slots: [{id: 's1', x: 35, y: 12}, {id: 's2', x: 58, y: 80}]
+  }, 
+  {
+
+  }
+]
