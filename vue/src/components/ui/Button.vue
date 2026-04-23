f421d62a4e111c049f2bf57cf62184bca70ca1de
Никита <aleksej@iMac-Aleksej.local>
2026-04-23 03:14:16 +0300

    adding a timer, leaderboard and hard mode

---
+++ vue/src/components/ui/Button.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Button"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="scss">
+
+</style>
