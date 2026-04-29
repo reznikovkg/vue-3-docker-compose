3b73a2d623706882aee758c20e5d26575419e6f1
NezKap <werras206@mail.ru>
2026-04-30 02:55:15 +0300

    Изменена механика положения персонажа: теперь он всегда в центре, а остальные объекты смещаются относительно него при его движении

--- vue/src/components/pages/GamePage.vue
+++ vue/src/components/pages/GamePage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="map" @mousemove="(e) => handleMouseCoords(e)">
-    <Bullet v-for="bullet in getBullets" :key="bullet.id" :x="bullet.x" :y="bullet.y" :id="bullet.id" />
-    <div class="map__player" :style="styles" />
-    <Enemy v-for="enemy in getEnemies" :key="enemy.id" :x="enemy.x" :y="enemy.y" :id="enemy.id" />
+    <Bullet v-for="bullet in getBullets" :key="bullet.id" :x="cameraOffsetX(bullet.x)" :y="cameraOffsetY(bullet.y)" :id="bullet.id" />
+    <div class="map__player" />
+    <Enemy v-for="enemy in getEnemies" :key="enemy.id" :x="cameraOffsetX(enemy.x)" :y="cameraOffsetY(enemy.y)" :id="enemy.id" />
     <div v-if="!getGameStatus" class="map__over">
       Игра окончена
       <button class="map__over__restart" @click="() => restart()">
@@ -28,6 +28,10 @@ export default {
       mouseCoords: {
         x: 0,
         y: 0
+      },
+      cameraCoords: {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
       }
     }
   },
@@ -38,12 +42,7 @@ export default {
       'getBullets',
       'getEnemies',
       'getGameStatus'
-    ]),
-    styles () {
-      return {
-        transform: `translate(-50%, -50%) translateX(${this.getCoords.x}px) translateY(${this.getCoords.y}px)`
-      }
-    }
+    ])
   },
   mounted () {
     window.addEventListener('keydown', (e) => this.pressedArrow(e))
@@ -65,26 +64,37 @@ export default {
       'moveEnemies',
       'setGameStatus'
     ]),
+    cameraOffsetX (x) {
+      return x - this.cameraCoords.x + window.innerWidth / 2
+    },
+    cameraOffsetY (y) {
+      return y - this.cameraCoords.y + window.innerHeight / 2
+    },
     pressedArrow (e) {
       if (!this.getGameStatus) {
         return
       }
-      if (e.key === 'ArrowRight') {
+      const step = 20
+      if (e.key === 'ArrowRight' && this.getCoords.x < window.innerWidth) {
         this.moveRight()
+        this.cameraCoords.x += step
       }
-      if (e.key === 'ArrowLeft') {
+      if (e.key === 'ArrowLeft' && this.getCoords.x > 0) {
         this.moveLeft()
+        this.cameraCoords.x -= step
       }
-      if (e.key === 'ArrowUp') {
+      if (e.key === 'ArrowUp' && this.getCoords.y > 0) {
         this.moveUp()
+        this.cameraCoords.y -= step
       }
-      if (e.key === 'ArrowDown') {
+      if (e.key === 'ArrowDown' && this.getCoords.y < window.innerHeight) {
         this.moveDown()
+        this.cameraCoords.y += step
       }
     },
     handleMouseCoords (e) {
-      this.mouseCoords.x = e.clientX
-      this.mouseCoords.y = e.clientY
+      this.mouseCoords.x = e.clientX + this.cameraCoords.x - window.innerWidth / 2
+      this.mouseCoords.y = e.clientY + this.cameraCoords.y - window.innerHeight / 2
     },
     bulletMovement () {
       setInterval(() => {
@@ -135,6 +145,9 @@ export default {
     background: red;
     width: 80px;
     height: 80px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     transition: 0.2s;
   }
 
