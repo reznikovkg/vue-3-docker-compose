158c49ad8084492c772adb5560dbcf0f7750af79
Liza <liza.kovtun.2004@mail.ru>
2026-05-14 19:46:20 +0300

    feat: init game page and router

---
+++ vue/src/components/pages/GamePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="game-page">
+    <h1 class="game-page__title">
+      Составь пару
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GamePage',
+}
+</script>
